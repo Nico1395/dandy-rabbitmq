5375849be3b14bb935fe3af3b0419d5e1b93d4b2
nico1395 <119791859+Nico1395@users.noreply.github.com>
2026-09-04 19:28:40 +0200

    default fixtures

---
+++ src/DandyRabbitMQ.Serialization.SystemTextJson.Tests/Fixtures/DefaultFixture.cs
@@ -0,0 +1,27 @@
+using Microsoft.Extensions.DependencyInjection;
+
+namespace DandyRabbitMQ.Serialization.SystemTextJson.Tests.Fixtures;
+
+public sealed class DefaultFixture : IServiceProvider
+{
+    private readonly IServiceProvider _serviceProvider;
+    
+    public DefaultFixture()
+    {
+        var services = new ServiceCollection();
+
+        services.AddDandyRabbitMQSerialization(config => config.UseSystemTextJson());
+
+        _serviceProvider = services.BuildServiceProvider();
+    }
+
+    public object? GetService(Type serviceType)
+    {
+        return _serviceProvider.GetService(serviceType);
+    }
+    
+    public IPayloadSerializer GetPayloadSerializer()
+    {
+        return _serviceProvider.GetRequiredService<IPayloadSerializer>();
+    }
+}
