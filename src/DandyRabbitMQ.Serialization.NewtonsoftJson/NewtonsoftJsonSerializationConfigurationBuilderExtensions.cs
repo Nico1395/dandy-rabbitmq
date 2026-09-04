b337f1318ac718c4e2350db5d4457bc5c7a1d8ab
nico1395 <119791859+Nico1395@users.noreply.github.com>
2026-09-04 19:28:57 +0200

    making the config action optional

--- src/DandyRabbitMQ.Serialization.NewtonsoftJson/NewtonsoftJsonSerializationConfigurationBuilderExtensions.cs
+++ src/DandyRabbitMQ.Serialization.NewtonsoftJson/NewtonsoftJsonSerializationConfigurationBuilderExtensions.cs
@@ -2,7 +2,7 @@ namespace DandyRabbitMQ.Serialization.NewtonsoftJson;
 
 public static class NewtonsoftJsonSerializationConfigurationBuilderExtensions
 {
-    public static SerializationConfigurationBuilder UseNewtonsoftJson(this SerializationConfigurationBuilder serializationConfigurationBuilder, Action<NewtonsoftJsonPayloadSerializerConfigurationBuilder>? builderAction)
+    public static SerializationConfigurationBuilder UseNewtonsoftJson(this SerializationConfigurationBuilder serializationConfigurationBuilder, Action<NewtonsoftJsonPayloadSerializerConfigurationBuilder>? builderAction = null)
     {
         var builder = new NewtonsoftJsonPayloadSerializerConfigurationBuilder();
         builderAction?.Invoke(builder);
