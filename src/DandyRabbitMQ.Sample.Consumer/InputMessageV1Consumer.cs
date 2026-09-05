66f1d1e17bc1865c1fa637e269da38f11c48c8d8
nico1395 <119791859+Nico1395@users.noreply.github.com>
2026-09-05 21:34:32 +0200

    sample consumer updated

--- src/DandyRabbitMQ.Sample.Consumer/InputMessageV1Consumer.cs
+++ src/DandyRabbitMQ.Sample.Consumer/InputMessageV1Consumer.cs
@@ -5,7 +5,7 @@ namespace DandyRabbitMQ.Sample.Consumer;
 
 internal sealed class InputMessageV1Consumer : IConsumer<InputMessageV1>
 {
-    public Task<ConsumerResult> ConsumeAsync(InputMessageV1 message, CancellationToken cancellationToken)
+    public Task<ConsumerResult> ConsumeAsync(InputMessageV1 message, ConsumerContext context, CancellationToken cancellationToken)
     {
         Console.WriteLine($"Message received: {message.Text}");
         return Task.FromResult(ConsumerResult.Ack());
